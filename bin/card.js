444414238ea77abc0c662e4b5cabc4c7d977c36f
Vaibhav Mishra <vinu76jsr@gmail.com>
2022-03-03 21:35:46 +0530

    Update card.js

--- bin/card.js
+++ bin/card.js
@@ -16,21 +16,21 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Brad Westfall'),
-  email: chalk.cyan('brad@azpixels.com'),
-  work: chalk.white('JavaScript/React and CSS: Consulting and Training'),
-  twitter: chalk.cyan('https://twitter.com/bradwestfall'),
-  github: chalk.cyan('https://github.com/bradwestfall'),
-  consulting: chalk.cyan('http://azpixels.com'),
-  training: chalk.cyan('https://reacttraining.com'),
-  personal: chalk.cyan('http://bradwestfall.com'),
-  card: chalk.cyan('https://github.com/bradwestfall/npx-bradwestfall'),
+  name: chalk.white('Vaibhav Mishra'),
+  email: chalk.cyan('contact@vaibhavmishra.com'),
+  work: chalk.white('Full Stack Engineer - Microsoft'),
+  twitter: chalk.cyan('https://twitter.com/vinu76jsr'),
+  github: chalk.cyan('https://github.com/vinu76jsr'),
+//   consulting: chalk.cyan('http://azpixels.com'),
+//   training: chalk.cyan('https://reacttraining.com'),
+  personal: chalk.cyan('http://vaibhavmishra.com'),
+  card: chalk.cyan('https://github.com/vinu76jsr/npx-vaibhavmishra'),
 
   labelWork: chalk.white.bold('       Work:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
   labelGitHub: chalk.white.bold('     GitHub:'),
-  labelConsulting: chalk.white.bold(' Consulting:'),
-  labelTraining: chalk.white.bold('   Training:'),
+//   labelConsulting: chalk.white.bold(' Consulting:'),
+//   labelTraining: chalk.white.bold('   Training:'),
   labelPersonal: chalk.white.bold('   Personal:'),
   labelCard: chalk.white.bold('       Card:'),
 }
@@ -40,8 +40,8 @@ const heading = `${data.name} ${data.email}`
 const working = `${data.labelWork}  ${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
-const consulting = `${data.labelConsulting}  ${data.consulting}`
-const training = `${data.labelTraining}  ${data.training}`
+// const consulting = `${data.labelConsulting}  ${data.consulting}`
+// const training = `${data.labelTraining}  ${data.training}`
 const personal = `${data.labelPersonal}  ${data.personal}`
 const card = `${data.labelCard}  ${data.card}`
 
@@ -50,8 +50,8 @@ const output = heading + '\n\n' +
   working + '\n' +
   twittering + '\n' +
   githubing + '\n' +
-  consulting + '\n' +
-  training + '\n' +
+//   consulting + '\n' +
+//   training + '\n' +
   personal + '\n' +
   card
 
